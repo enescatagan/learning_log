3d74edfe0e175b2645d6fb6e1b664b38f6f8036f
enescatagan <enescatagan@gmail.com>
2023-02-20 15:16:23 +0300

    Map URL for learning_logs App

---
+++ learning_logs/urls.py
@@ -0,0 +1,11 @@
+"""Defines URL patterns for learning_logs"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+url_patterns = [
+    # Home Page
+    path('', views.index, name='index'),
+]
