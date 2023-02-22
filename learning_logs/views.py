2e573ba5420bce46120afaf15a28bf15f6836898
enescatagan <enescatagan@gmail.com>
2023-02-22 11:40:08 +0300

    Add new Topic, for user end; url--view--template

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 
 def index(request):
@@ -21,3 +22,21 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Add a new topic"""
+    # Determine request method; If doesn't POST it's probably GET
+    if request.method != 'POST':
+        # No data submitted; create a blank form
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Display a black or invalid form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
